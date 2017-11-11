272054ca77b4e86a26e6a6b887527220b1ad56f9
Jacket-Chan <31905180+Jacket-Chan@users.noreply.github.com>
2017-11-11 13:40:05 -1000

    Update content.js

--- content.js
+++ content.js
@@ -3,13 +3,14 @@ chrome.runtime.sendMessage({alexa: "gimme_the_url"}, function(response) {
   var url;
   url = response.haveit;
   xhttp.open("GET","https://web.archive.org/save/"+url,true);
-  xhttp.setRequestHeader("Accept-Encoding","gzip, deflate, br");
-  xhttp.setRequestHeader("Host","web.archive.org");
+  /*xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+  };*/
   xhttp.setRequestHeader("Accept-Language","en-US,en;q=0.9");
   xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");
-  xhttp.setRequestHeader("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3264.0 Safari/537.36");
   xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
   xhttp.setRequestHeader("Cache-Control","max-age=0");
-  xhttp.setRequestHeader("Connection","keep-alive");
   xhttp.send();
 });
