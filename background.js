20b1349bd3caeb0d0c4c10a3724224cd3dddb957
Jacket-Chan <31905180+Jacket-Chan@users.noreply.github.com>
2017-11-10 22:35:29 -1000

    Update background.js

--- background.js
+++ background.js
@@ -1,16 +1,16 @@
-/*chrome.runtime.onMessage.addListener(
+chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
     if (request.alexa == "gimme_the_url")
       sendResponse({haveit: sender.tab.url});
-  });*/
-  chrome.runtime.onMessage.addListener(
+  });
+  /*chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       console.log(sender.tab ?
                   "from a content script:" + sender.tab.url :
                   "from the extension");
       if (request.greeting == "hello")
         sendResponse({farewell: "goodbye"});
-    });
+    });*/
