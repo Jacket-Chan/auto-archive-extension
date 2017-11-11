93b9d9c09aaa38920f22763e425352e6ab380a12
Jacket-Chan <31905180+Jacket-Chan@users.noreply.github.com>
2017-11-10 21:54:48 -1000

    Update background.js

--- literally_just_reformatting_the_userscript/background.js
+++ literally_just_reformatting_the_userscript/background.js
@@ -1,5 +1,8 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-    console.log(response.farewell);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
   });
-});
