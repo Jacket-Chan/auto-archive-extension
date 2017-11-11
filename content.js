75ba642da822e5f2e5abf2ff4491a3039cc2d7ab
Jacket-Chan <31905180+Jacket-Chan@users.noreply.github.com>
2017-11-10 22:58:07 -1000

    Update content.js

--- content.js
+++ content.js
@@ -4,5 +4,7 @@ chrome.runtime.sendMessage({alexa: "gimme_the_url"}, function(response) {
   url = response.haveit;
   console.log(typeof url);
 });
+console.log(url);
 xhttp.open("GET","https://web.archive.org/save/"+url,true);
 xhttp.send();
+console.log(url);
