d828aea33096235300913f3289e6c1c8992f7e07
Jacket-Chan <31905180+Jacket-Chan@users.noreply.github.com>
2017-11-10 22:03:02 -1000

    Update background.js

--- literally_just_reformatting_the_userscript/background.js
+++ literally_just_reformatting_the_userscript/background.js
@@ -3,6 +3,6 @@ chrome.runtime.onMessage.addListener(
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-    if (request.alexa == "gimme_the_url")
-      sendResponse({haveit: sender.tab.url});
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
   });
