75af369e39ee42694754641e4ff97682b38c8b52
Jacket-Chan <31905180+Jacket-Chan@users.noreply.github.com>
2017-11-10 21:27:47 -1000

    Update content.js

--- literally_just_reformatting_the_userscript/content.js
+++ literally_just_reformatting_the_userscript/content.js
@@ -1,5 +1,9 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open("GET","https://web.archive.org/save/"+location.href,true);
+var url;
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    url = tabs[0].url;
+});
+xhttp.open("GET","https://web.archive.org/save/"+url,true);
 xhttp.send();
 xhttp.open("GET","https://maker.ifttt.com/trigger/http_request_test/with/key/cysTImCkywHvyn_o9_lYyu",true);
 xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
