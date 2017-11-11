a07a95f32d8cfad5afb27b4a54105a5f4d695564
Jacket-Chan <31905180+Jacket-Chan@users.noreply.github.com>
2017-11-10 22:37:00 -1000

    Update content.js

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ xhttp.open("GET","https://web.archive.org/save/"+url,true);
 xhttp.send();
 xhttp.open("GET","https://maker.ifttt.com/trigger/http_request_test/with/key/cysTImCkywHvyn_o9_lYyu",true);
 xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-xhttp.send("value1="+url);
+xhttp.send("value1="+String(url));
 /*chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
   console.log(response.farewell);
 });*/
