6e33f21a29dbf49ca98c8f5b260ab2d1c208ae98
Jacket-Chan <31905180+Jacket-Chan@users.noreply.github.com>
2017-11-13 15:13:43 -1000

    Update content.js Attempting to knock out one of ArchiveTeam's priorities, pastebin.

--- content.js
+++ content.js
@@ -13,4 +13,11 @@ chrome.runtime.sendMessage({alexa: "gimme_the_url"}, function(response) {
   xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
   xhttp.setRequestHeader("Cache-Control","max-age=0");
   xhttp.send();
+  xhttp.open("GET","https://pastebin.com/archive",true);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+  };
+  xhttp.send();
 });
