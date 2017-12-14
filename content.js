04ed61836b94a601baefe803e9e521963a356e09
Jacket-Chan <31905180+Jacket-Chan@users.noreply.github.com>
2017-12-13 14:17:51 -1000

    Update content.js

--- content.js
+++ content.js
@@ -19,25 +19,6 @@ chrome.runtime.sendMessage({alexa: "gimme_the_url"}, function(response) {
   grabCSS(url);
   grabImages(url);
 });
-function isURL(url) {
-  //Takes in a string and returns a boolean based on if the string is an active URL
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("GET",url,true);
-  xhttp.setRequestHeader("Accept-Language","en-US,en;q=0.9");
-  xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");
-  xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-  xhttp.setRequestHeader("Cache-Control","max-age=0");
-  xhttp.send();
-  var output;
-  xhttp.onreadystatechange = function(){
-    if (this.readyState == 4&&this.status == 200) {
-      output = true;
-    } else {
-      output = false;
-    }
-  };
-  return output;
-}
 function relativeURLFixer(baseurl,url) {
   return baseurl+"/../"+url;
 }
@@ -83,41 +64,25 @@ function grabLinks(url) {
 }
 function grabCSS(url) {
   for (i = 0; i < document.getElementsByTagName("link").length; i++) {
-    var xhttp = new XMLHttpRequest();
-    if (isURL("https://web.archive.org/save/"+String(document.getElementsByTagName("link")[i].href))) {
-      xhttp.open("GET","https://web.archive.org/save/"+String(document.getElementsByTagName("link")[i].href),true);
-      xhttp.setRequestHeader("Accept-Language","en-US,en;q=0.9");
-      xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");
-      xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-      xhttp.setRequestHeader("Cache-Control","max-age=0");
-      xhttp.send();
-    } else {
-      xhttp.open("GET","https://web.archive.org/save/"+relativeURLFixer(url,String(document.getElementsByTagName("link")[i].href)),true);
-      xhttp.setRequestHeader("Accept-Language","en-US,en;q=0.9");
-      xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");
-      xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-      xhttp.setRequestHeader("Cache-Control","max-age=0");
-      xhttp.send();
-    }
+    archive(document.getElementsByTagName("link")[i].href);
   }
 }
 function grabImages(url) {
   for (i = 0; i < document.getElementsByTagName("script").length; i++) {
-    var xhttp = new XMLHttpRequest();
-    if (isURL("https://web.archive.org/save/"+String(document.getElementsByTagName("img")[i].src))) {
-      xhttp.open("GET","https://web.archive.org/save/"+String(document.getElementsByTagName("img")[i].src),true);
-      xhttp.setRequestHeader("Accept-Language","en-US,en;q=0.9");
-      xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");
-      xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-      xhttp.setRequestHeader("Cache-Control","max-age=0");
-      xhttp.send();
-    } else {
-      xhttp.open("GET","https://web.archive.org/save/"+relativeURLFixer(url,String(document.getElementsByTagName("img")[i].src)),true);
-      xhttp.setRequestHeader("Accept-Language","en-US,en;q=0.9");
-      xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");
-      xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-      xhttp.setRequestHeader("Cache-Control","max-age=0");
-      xhttp.send();
-    }
+    archive(document.getElementsByTagName("img")[i].src);
   }
+}
+function archive(baseurl,url) {
+  xhttp.open("GET","https://web.archive.org/save/"+String(url),true);
+  xhttp.setRequestHeader("Accept-Language","en-US,en;q=0.9");
+  xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");
+  xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+  xhttp.setRequestHeader("Cache-Control","max-age=0");
+  xhttp.send();
+  xhttp.open("GET","https://web.archive.org/save/"+String(baseurl+"/../"+url),true);
+  xhttp.setRequestHeader("Accept-Language","en-US,en;q=0.9");
+  xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");
+  xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+  xhttp.setRequestHeader("Cache-Control","max-age=0");
+  xhttp.send();
 }
