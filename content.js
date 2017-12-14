ca4babd78cdf5cf30798218959bb9a4616899530
Jacket-Chan <31905180+Jacket-Chan@users.noreply.github.com>
2017-12-13 14:56:36 -1000

    Update content.js

--- content.js
+++ content.js
@@ -11,34 +11,34 @@ chrome.runtime.sendMessage({alexa: "gimme_the_url"}, function(response) {
   xhttp.send();
   grabLinks(list);
 });
-setInterval(function(){
+/*setInterval(function(){
   var xhttp = new XMLHttpRequest();
   xhttp.open("GET","https://pastebin.com/archive",true);
   xhttp.setRequestHeader("Accept-Language","en-US,en;q=0.9");
-  xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");
-  xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-  xhttp.setRequestHeader("Cache-Control","max-age=0");
+  xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");*/
+  //xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+  /*xhttp.setRequestHeader("Cache-Control","max-age=0");
   xhttp.send();
   xhttp.onreadystatechange = function(){
     var list = this.responseText;
     grabLinks(list);
   };
-},10000);
-function grabLinks(list) {
-  for (i = 0; i < list.getElementsByTagName("a").length; i++) {
-    archive(list,list.getElementsByTagName("a")[i].href);
+},10000);*/
+function grabLinks(/*list*/) {
+  for (i = 0; i < /*list*/document.getElementsByTagName("a").length; i++) {
+    archive(/*list,list*/document.getElementsByTagName("a")[i].href);
   }
-  for (i = 0; i < list.getElementsByTagName("script").length; i++) {
-    archive(list,list.getElementsByTagName("script")[i].src);
+  for (i = 0; i < /*list*/document.getElementsByTagName("script").length; i++) {
+    archive(/*list,list*/document.getElementsByTagName("script")[i].src);
   }
-  for (i = 0; i < list.getElementsByTagName("link").length; i++) {
-    archive(list,list.getElementsByTagName("link")[i].href);
+  for (i = 0; i < /*list*/document.getElementsByTagName("link").length; i++) {
+    archive(/*list,list*/document.getElementsByTagName("link")[i].href);
   }
-  for (i = 0; i < list.getElementsByTagName("script").length; i++) {
-    archive(list,list.getElementsByTagName("img")[i].src);
+  for (i = 0; i < /*list*/document.getElementsByTagName("script").length; i++) {
+    archive(/*list,list*/document.getElementsByTagName("img")[i].src);
   }
 }
-function archive(page,url) {
+function archive(/*page,*/url) {
   xhttp.open("GET","https://web.archive.org/save/"+String(url),true);
   xhttp.setRequestHeader("Accept-Language","en-US,en;q=0.9");
   xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");
