16a48bdfe8c6dae8a1249b18915669a1e9e3fc98
Jacket-Chan <31905180+Jacket-Chan@users.noreply.github.com>
2017-12-13 15:14:28 -1000

    Update content.js

--- content.js
+++ content.js
@@ -11,18 +11,18 @@ chrome.runtime.sendMessage({alexa: "gimme_the_url"}, function(response) {
   xhttp.send();
   grabLinks(list);
 });
-setInterval(function(){
+/*setInterval(function(){
   var xhttp = new XMLHttpRequest();
   xhttp.open("GET","https://pastebin.com/archive",true);
   xhttp.setRequestHeader("Accept-Language","en-US,en;q=0.9");
-  xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");
-  xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-  xhttp.setRequestHeader("Cache-Control","max-age=0");
+  xhttp.setRequestHeader("Upgrade-Insecure-Requests","1");*/
+  //xhttp.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+  /*xhttp.setRequestHeader("Cache-Control","max-age=0");
   xhttp.send();
   xhttp.onreadystatechange = function(){
     grabLinks(this.responseText);
   };
-},10000);
+},10000);*/
 function grabLinks(list) {
   for (i = 0; i < (list.getElementsByTagName("a").length - 1); i++) {
     archive(list,list.getElementsByTagName("a")[i].href);
